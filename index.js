30fa3f61b876cec3d704ec984b586195e52bcfc3
Enzo <kdenzo88@gmail.com>
2022-05-12 10:26:54 +0200

    Test

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
  const createIndexFile = () => {
-    if  ( ! fs . existsSync ( 'build' ) )  {
-        fs . mkdirSync ( 'construire' )
+    if  ( !fs.existsSync ( 'build' ) )  {
+        fs.mkdirSync ( 'construire' )
     }
-    fs . writeFileSync ( 'build/index.html' ,  'Hello bonjour ' ) ;
-    fs . writeFileSync ( 'build/index.html' ,  'Hello world ' ) ;
+    fs.writeFileSync ( 'build/index.html' ,  'Hello bonjour ' ) ;
+    fs.writeFileSync ( 'build/index.html' ,  'Hello world ' ) ;
 } ;
 
 createIndexFile ( ) ;
