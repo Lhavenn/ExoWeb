026d11670eb1f9e81720b1d5549a95b5f03aa849
Enzo <kdenzo88@gmail.com>
2022-05-12 10:32:39 +0200

    Test

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs');
     if  ( !fs.existsSync ( 'build' ) )  {
         fs.mkdirSync ( 'construire' )
     }
+    
     fs.writeFileSync ( 'build/index.html' ,  'Hello bonjour ' ) ;
     fs.writeFileSync ( 'build/index.html' ,  'Hello world ' ) ;
 } ;
